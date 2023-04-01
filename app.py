421f8c98dfd89dd2c4d7a5c4836f0d46858e943c
lera <120723754+Ltr00fim@users.noreply.github.com>
2023-04-01 12:33:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_restx import Api
 from config import Config
-from dao.model.user import User
 from setup_db import db
 from views.auth import auth_ns
 from views.directors import director_ns
@@ -13,6 +12,7 @@ from views.users import user_ns
 def create_app(config_object):
     app = Flask(__name__)
     app.config.from_object(config_object)
+    app.app_context().push()
     register_extensions(app)
     return app
 
@@ -25,18 +25,9 @@ def register_extensions(app):
     api.add_namespace(movie_ns)
     api.add_namespace(auth_ns)
     api.add_namespace(user_ns)
-    with app.app_context():
-        db.create_all()
-        u1 = User(username="vasya", password="my_little_pony", role="user")
-        u2 = User(username="oleg", password="qwerty", role="user")
-        u3 = User(username="oleg", password="P@ssw0rd", role="admin")
-
-        with db.session.begin():
-            db.session.add_all([u1, u2, u3])
 
 
-app = create_app(Config())
-app.debug = True
+app = create_app(Config)
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=10001, debug=True)
+    app.run(debug=True)

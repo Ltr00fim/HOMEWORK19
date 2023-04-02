b45da62e99869c64abb49d57e2cf467fce4f4c65
lera <120723754+Ltr00fim@users.noreply.github.com>
2023-04-02 10:09:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,8 @@ def register_extensions(app):
 
 app = create_app(Config)
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="localhost", port=10001)

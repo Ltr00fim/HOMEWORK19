4d127d14730c1195d05dab8ba829cdcb06d1f651
lera <120723754+Ltr00fim@users.noreply.github.com>
2023-04-02 10:13:47 +0300

    Update user.py

--- service/user.py
+++ service/user.py
@@ -2,22 +2,25 @@ import hashlib
 import base64
 import hmac
 from dao.user import UserDAO
-from constants import PWD_HASH_SALT, PWD_HASH_ITERATIONS
+from config import Config
 
 
 class UserService:
     def __init__(self, dao: UserDAO):
         self.dao = dao
 
-    def get_name(self, name):
-        return self.dao.get_name(name)
+    def get_one(self, pk):
+        return self.dao.get_one(pk)
+
+    def get_by_username(self, username):
+        return self.dao.get_by_username(username)
 
     def get_all(self):
         return self.dao.get_all()
 
-    def create(self, user_d):
-        user_d['password'] = self.get_hash(user_d['password'])
-        return self.dao.create(user_d)
+    def create(self, data):
+        data['password'] = self.get_hash(data['password'])
+        return self.dao.create(data)
 
     def update(self, id):
         self.dao.update(id)
@@ -26,16 +29,23 @@ class UserService:
     def delete(self, rid):
         self.dao.delete(rid)
 
-    def get_hash(password):
-        return base64.b64decode(hashlib.pbkdf2_hmac(
+    def get_hash(self, password):
+        hash_digest = hashlib.pbkdf2_hmac(
             'sha256',
             password.encode('utf-8'),
-            PWD_HASH_SALT,
-            PWD_HASH_ITERATIONS
-        ))
-
-    def compare_hash(self, password_hash, other_password):
-        return hmac.compare_digest(
-            base64.b64decode(password_hash),
-            hashlib.pbkdf2_hmac('sha256', other_password.encode(), PWD_HASH_SALT, PWD_HASH_ITERATIONS)
+            Config.PWD_HASH_SALT,
+            Config.PWD_HASH_ITERATIONS
         )
+        return base64.b64encode(hash_digest)
+
+    def compare_password(self, password_hash, other_password):
+        decoded_digist = base64.b64decode(password_hash)
+
+        hash_digest = hashlib.pbkdf2_hmac(
+            "sha256",
+            other_password.encode("utf-8"),
+            Config.PWD_HASH_SALT,
+            Config.PWD_HASH_ITERATIONS
+        )
+
+        return hmac.compare_digest(decoded_digist, hash_digest)

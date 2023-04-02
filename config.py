d7ad1771d37e67969861a85744166c58f146f9b1
lera <120723754+Ltr00fim@users.noreply.github.com>
2023-04-02 10:09:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 class Config(object):
     DEBUG = True
     SECRET_HERE = '249y823r9v8238r9u'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///./movies.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PWD_HASH_SALT = b'2423r3ef3r23232edasfdsfwdswdw2e23'
+    PWD_HASH_ITERATIONS = 100_000
+    JWT_SECRET = "SECRET"
+    JWT_ALGORITHM = "HS256"

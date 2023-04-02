e82e79ca2b8e1c4cfabfb99654f7940baa12f195
lera <120723754+Ltr00fim@users.noreply.github.com>
2023-04-02 10:12:08 +0300

    Update genres.py

--- views/genres.py
+++ views/genres.py
@@ -1,7 +1,7 @@
 from flask_restx import Resource, Namespace
 from flask import request
 from dao.model.genre import GenreSchema
-from decorators import admin_reguired, auth_reguired
+from decorators import auth_required, admin_required
 from implemented import genre_service
 
 genre_ns = Namespace('genres')
@@ -9,12 +9,12 @@ genre_ns = Namespace('genres')
 
 @genre_ns.route('/')
 class GenresView(Resource):
-    @auth_reguired
+    @auth_required
     def get(self):
         data = genre_service.get_all()
         return GenreSchema(many=True).dump(data), 200
 
-    @admin_reguired
+    @admin_required
     def post(self):
         data = genre_service.create(request.json)
         return GenreSchema(many=False).dump(data), 201
@@ -22,17 +22,17 @@ class GenresView(Resource):
 
 @genre_ns.route('/<id>')
 class GenreView(Resource):
-    @auth_reguired
+    @auth_required
     def get(self, id):
         data = genre_service.get_one(int(id))
         return GenreSchema().dump(data), 200
 
-    @admin_reguired
+    @admin_required
     def delete(self, id):
         genre_service.delete(int(id))
         return "", 204
 
-    @admin_reguired
+    @admin_required
     def update(self, id):
         genre_service.update(int(id), request.json)
         return "", 200
